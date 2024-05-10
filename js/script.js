cb4b6b01e46dde1d3e10d9241c10b82aab1b540a
Adam-Batiuk <159185155+Adam-Batiuk@users.noreply.github.com>
2024-05-10 18:40:58 +0000

    done

--- js/script.js
+++ js/script.js
@@ -9,26 +9,21 @@ function myButtonClicked() {
   // define numbers
   const firstNumber = parseFloat(document.getElementById("first-number").value)
   const secondNumber = parseFloat(document.getElementById("second-number").value)
-  let biggerNumber = 0
-  let smallerNumber = 0
-  if (firstNumber > secondNumber) {
-    biggerNumber = firstNumber
-    smallerNumber = secondNumber
-  } else {
-    biggerNumber = secondNumber
-    smallerNumber = firstNumber
-  }
-  let remainder = biggerNumber
+  let dividend = firstNumber
+  let divisor = secondNumber
+  let remainder = 0
   // divide
   let numberOfLoops = 0
+  let answer = 0
   while (true) {
-    remainder = remainder - smallerNumber
-    numberOfLoops++
-    if (remainder < smallerNumber) {
+    if (dividend < divisor) {
+      remainder = dividend
       break
     }
+    dividend = dividend - divisor
+    numberOfLoops++
   }
-  const answer = numberOfLoops
+  answer = numberOfLoops
   // print answer
   document.getElementById("answer").innerHTML = "The answer is: " + answer + " R " + remainder
 }
