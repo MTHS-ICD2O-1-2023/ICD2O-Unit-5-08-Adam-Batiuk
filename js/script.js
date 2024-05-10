89ab04890b3a511ceb2226e2715b79043564e726
Adam-Batiuk <159185155+Adam-Batiuk@users.noreply.github.com>
2024-05-10 17:57:01 +0000

    html

--- js/script.js
+++ js/script.js
@@ -1,5 +1,21 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2024 Mr. Adam Batiuk All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Adam Batiuk
+// Created on: Mar 2024
 // This file contains the JS functions for index.html
+
+
+function myButtonClicked() {
+  // define numbers
+  const firstNumber = parseFloat(document.getElementById("first-number").value)
+  const secondNumber = parseFloat(document.getElementById("second-number").value)
+  let answer = 0
+  // multiply
+  let numberOfLoops = 1
+  while (numberOfLoops <= firstNumber) {
+    answer = answer + secondNumber
+    numberOfLoops++
+  }
+  // print answer
+  document.getElementById("answer").innerHTML = "The answer is: " + answer
+}
